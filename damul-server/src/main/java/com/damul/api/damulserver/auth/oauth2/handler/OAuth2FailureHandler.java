fa67c2456fa1731782cea58e71566df28cdca15d
지유림 <zyu22@naver.com>
2025-01-24 16:26:35 +0900

    Feat: Add Spring Security configuration with OAuth2 and JWT [지유림]

--- damul-server/src/main/java/com/damul/api/damulserver/auth/oauth2/handler/OAuth2FailureHandler.java
+++ damul-server/src/main/java/com/damul/api/damulserver/auth/oauth2/handler/OAuth2FailureHandler.java
@@ -1,4 +1,22 @@
 package com.damul.api.damulserver.auth.oauth2.handler;
 
-public class OAuth2FailureHandler {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class OAuth2FailureHandler implements AuthenticationFailureHandler {
+
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+
+    }
 }
--- damul-server/src/main/java/com/damul/api/damulserver/auth/oauth2/handler/OAuth2SuccessHandler.java
+++ damul-server/src/main/java/com/damul/api/damulserver/auth/oauth2/handler/OAuth2SuccessHandler.java
@@ -1,4 +1,21 @@
 package com.damul.api.damulserver.auth.oauth2.handler;
 
-public class OAuth2SuccessHandler {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+    }
 }
