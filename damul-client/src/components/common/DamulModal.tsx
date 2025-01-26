061e4fe18b6a42e44d558e6d65633590af73c1bc
신성우 <asdfgg77@naver.com>
2025-01-26 18:20:56 +0900

    Feat : Add Modal component - add related dependencies - add dialog ui component

---
+++ damul-client/src/components/common/DamulModal.tsx
@@ -0,0 +1,57 @@
+import { Dispatch, ReactNode, SetStateAction } from "react";
+import clsx from "clsx";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+interface DamulModalProps {
+  isOpen: boolean; // 모달의 열림 상태
+  setIsOpen: Dispatch<SetStateAction<boolean>>; // 부모에서 모달 상태를 제어하는 함수
+  triggerComponent: ReactNode; // 모달창을 열기 위한 컴포넌트 요소
+  contentStyle?: string; // 모달창 전체 스타일
+  headerStyle?: string; // 모달창 헤더 부분 스타일
+  titleStyle?: string; // 모달창 제목 텍스트 스타일
+  title: string; // 모달창 제목 텍스트
+  children: ReactNode; // 모달창 본문 요소
+  footerComponent?: ReactNode; // 모달창 하단 컴포넌트 요소
+}
+
+const defaultContentStyle = "p-4 border-0 max-w-72 rounded-xl";
+const defaultHeaderStyle = "pb-3 border-b-2 border-b-[#DEDEDE]";
+const defaultTitleStyle = "text-positive-300 text-xl";
+
+const DamulModal = ({
+  isOpen,
+  setIsOpen,
+  triggerComponent,
+  contentStyle,
+  headerStyle,
+  titleStyle,
+  title,
+  children,
+  footerComponent,
+}: DamulModalProps) => {
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>{triggerComponent}</DialogTrigger>
+      <DialogContent className={clsx(defaultContentStyle, contentStyle)}>
+        <DialogHeader className={clsx(defaultHeaderStyle, headerStyle)}>
+          <DialogTitle className={clsx(defaultTitleStyle, titleStyle)}>
+            {title}
+          </DialogTitle>
+        </DialogHeader>
+        <DialogDescription>{children}</DialogDescription>
+        {footerComponent && <DialogFooter>{footerComponent}</DialogFooter>}
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default DamulModal;
