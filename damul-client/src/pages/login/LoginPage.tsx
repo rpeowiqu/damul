55f379519890a2818e1af1d8930126ab9a4823ac
전종우 <jinlaove17@naver.com>
2025-01-26 13:00:34 +0900

    Feat : Add social login [전종우] - add bound animation to slogan

--- damul-client/src/pages/login/LoginPage.tsx
+++ damul-client/src/pages/login/LoginPage.tsx
@@ -4,15 +4,31 @@ import GoogleIcon from "@/components/svg/GoogleIcon";
 import NaverIcon from "@/components/svg/NaverIcon";
 
 const LoginPage = () => {
+  const onClickKakaoLogin = () => {
+    window.location.href = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${
+      import.meta.env.VITE_KAKAO_APP_KEY
+    }&redirect_uri=${import.meta.env.VITE_KAKAO_REDIRECT_URI}`;
+  };
+
+  const onClickGoogleLogin = () => {
+    window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${import.meta.env.VITE_GOOGLE_CLIENT_ID}&redirect_uri=${import.meta.env.VITE_GOOGLE_REDIRECT_URI}&response_type=token&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile`;
+  };
+
+  const onClickNaverLogin = () => {
+    window.location.href = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${import.meta.env.VITE_NAVER_CLIENT_ID}&redirect_uri=${import.meta.env.VITE_NAVER_REDIRECT_URI}&state=damul`;
+  };
+
   return (
     <main className="px-10">
       <p className="text-4xl mt-40">
-        당신의 식자재 관리
+        효율적인 식자재 관리
         <br />
-        오직 <b>다믈램</b>에서
+        오직
+        <div className="inline-block animate-bounce m-2 font-bold">다믈램</div>
+        에서
       </p>
 
-      <div className="flex flex-col gap-5 mt-24">
+      <div className="flex flex-col gap-5 mt-36">
         <div className="flex items-center px-10">
           <hr className="flex-1" />
           <p className="text-normal-300 text-center mx-2">
@@ -27,7 +43,7 @@ const LoginPage = () => {
             variant="shadow"
             size="full"
             textSize="base"
-            onClick={() => {}}
+            onClick={onClickKakaoLogin}
           >
             카카오로 시작하기
           </DamulButton>
@@ -39,7 +55,7 @@ const LoginPage = () => {
             variant="shadow"
             size="full"
             textSize="base"
-            onClick={() => {}}
+            onClick={onClickGoogleLogin}
           >
             구글로 시작하기
           </DamulButton>
@@ -51,7 +67,7 @@ const LoginPage = () => {
             variant="shadow"
             size="full"
             textSize="base"
-            onClick={() => {}}
+            onClick={onClickNaverLogin}
           >
             네이버로 시작하기
           </DamulButton>
