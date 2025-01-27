38f2222b0d88018819bc8335fad42bb481ef2398
한재서 <hanahyun1@naver.com>
2025-01-27 16:42:34 +0900

    Feat: Add IngredientStorage ENUM[한재서]

--- damul-server/src/main/java/com/damul/api/DamulServerApplication.java
+++ damul-server/src/main/java/com/damul/api/DamulServerApplication.java
@@ -1,4 +1,4 @@
-package com.damul.api.damulserver;
+package com.damul.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
