63778b1079f4d3ce00d5ac593d9c5897195e45c0
한재서 <hanahyun1@naver.com>
2025-01-27 14:26:12 +0900

    Chore: disabled test environment[한재서]

--- damul-server/src/test/java/com/damul/api/damulserver/DamulServerApplicationTests.java
+++ damul-server/src/test/java/com/damul/api/damulserver/DamulServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.damul.api.damulserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = DamulServerApplicationTests.class)
+@SpringBootTest
+@ActiveProfiles("test")
 class DamulServerApplicationTests {
 	@Test
 	void contextLoads() {
