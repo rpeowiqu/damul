6f26b1b7d15118b4398e17857f537cf081276d06
한재서 <hanahyun1@naver.com>
2025-01-31 14:06:14 +0900

    Chore: Commit before push to server-develop[한재서]

--- damul-server/src/main/java/com/damul/api/main/repository/UserIngredientRepository.java
+++ damul-server/src/main/java/com/damul/api/main/repository/UserIngredientRepository.java
@@ -24,7 +24,7 @@ public interface UserIngredientRepository extends JpaRepository<UserIngredient, 
 
     @Query("SELECT new com.damul.api.main.dto.response.HomeIngredientDetail(" +
             "ui.userIngredientId, ui.categoryId, ui.ingredientQuantity, " +
-            "ui.ingredientUp, ui.ingredientName, ui.dueDate, ui.user.warning) " +  // user 관계 사용
+            "ui.ingredientUp, ui.ingredientName, ui.dueDate, ui.userReciept.warning) " +  // user 관계 사용
             "FROM UserIngredient ui " +
             "WHERE ui.userIngredientId = :ingredientId")
     HomeIngredientDetail findHomeIngredientDetailById(@Param("ingredientId") int ingredientId);
