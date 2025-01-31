74d0c07bc9afd388c4db9df864de02e4948f2b62
윤혜진 <dbsgpwls0316@naver.com>
2025-01-31 16:55:15 +0900

    Feat: Add HTTP request and response fuction [윤혜진]

--- damul-ocr/app/main.py
+++ damul-ocr/app/main.py
@@ -1,7 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from services.ocr_service import ocr_service_execution
 
 app = FastAPI() # 인스턴스 생성
 
+# 오류 방지용 엔드포인트
 @app.get("/")
 def root():
-    return 'Hello World!'
+    return 'Hello World!' + ' 동기 비동기 문제가 힘듭니다...'
+
+@app.post('/api/v1/home/ingredients/receipt')
+async def user_ingredients_ocr(image: UploadFile = File(...)):
+    try:
+        image_bytes = await image.read()
+
+        # ocr
+        extracted_text = await ocr_service_execution(image_bytes)
+        if not extracted_text[0]:
+            return { 'ERROR' : extracted_text[1] }
+
+        # gpt
+
+        # 응답
+        return 'ok~'
+
+    except Exception as e:
+        import traceback
+        error_details = traceback.format_exc()
+        return {'ERROR': 'INTERNAL_SERVER_ERROR', 'DETAILS': str(error_details)}
--- damul-ocr/app/services/ocr_service.py
+++ damul-ocr/app/services/ocr_service.py
@@ -1,13 +1,25 @@
 # OCR API 호출
 from paddleocr import PaddleOCR
 import cv2
+import numpy as np
+
+from PIL import Image
+import io
 
 import re   # 정규식
+
+import json
+
+
 
 # 속도 향상을 위한 정규식 컴파일
 HANGUL_REGEX = re.compile('[ㄱ-ㅎ가-힣]+')
 DIGIT_REGEX = re.compile(r'\d')
 LAST_NINE_REGEX = re.compile(r'(9)(?!.*\d)')
+
+# PaddleOCR 초기화
+ocr = PaddleOCR(lang='korean', use_mp=False)  # 'korean' 언어 설정
+
 
 # 9 -> g
 def data_cleansing_9_to_g(ocr_res):
@@ -17,32 +29,34 @@ def data_cleansing_9_to_g(ocr_res):
             return ocr_res[:match.start()] + 'g' + ocr_res[match.end():]
     return ocr_res
 
-# PaddleOCR 초기화
-ocr = PaddleOCR(lang='korean')  # 'korean' 언어 설정
 
-# 이미지 경로 설정
-image_path = ''  # 영수증 이미지 경로
+async def ocr_service_execution(image_bytes: bytes):
+    # 이미지 로드
+    image = Image.open(io.BytesIO(image_bytes))
+    if image is None:   # 이미지 없을 때 error 반환
+        return [0, 'image load fail']
 
-# 이미지 로드
-image = cv2.imread(image_path)
-if image is None:
-    print("ERROR: image load fail")
-    exit()
+    # 이미지 변환
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
-# OCR 수행
-results = ocr.ocr(image_path, cls=True)
+    # OCR 수행
+    ocr_results = ocr.ocr(image, cls=True)
 
-# 한글 텍스트 추출
-korean_texts = []
-for line in results[0]:
-    text, confidence = line[1]
-    text = data_cleansing_9_to_g(text)  # 9 -> g
-    korean_texts.append(text)
+    # 텍스트 추출
+    ocr_results_texts = []
+    for line in ocr_results[0]:
+        text, confidence = line[1]
+        text = data_cleansing_9_to_g(text)  # 9 -> g
+        ocr_results_texts.append(text)
 
-# 결과 출력
-print("추출된 한글 텍스트:")
-for text in korean_texts:
-    print(text)
+    # 결과 출력
+    # print("추출된 한글 텍스트:")
+    # for text in ocr_results_texts:
+    #     print(text)
 
-# print("***************results***************")
-# print(results)
+    # print("***************results***************")
+    # print(ocr_results)
+
+    # 결과 반환
+    return ocr_results_texts
+    # return json.dumps({'data': ocr_results_texts})
