822fc2f6743bb4c397783b546c0c526b374fbdc5
윤서희 <1015alicia@naver.com>
2025-02-04 09:28:57 +0900

    Feat : Add Icons for post page [윤서희]

--- damul-client/src/components/community/PostCard.tsx
+++ damul-client/src/components/community/PostCard.tsx
@@ -1,7 +1,23 @@
 import AlarmIcon from "../svg/AlarmIcon";
+import TitleIcon from "../svg/TitleIcon";
+import PictureIcon from "../svg/PictureIcon";
+import ContentIcon from "../svg/ContentIcon";
+import IngredientsIcon from "../svg/IngredientsIcon";
+import OrdersIcon from "../svg/OrdersIcon";
+import MemberIcon from "../svg/MemberIcon";
+
+const iconSelect = {
+  제목: <TitleIcon className="w-6 h-6" />,
+  사진: <PictureIcon className="w-6 h-6" />,
+  소개: <ContentIcon className="w-7 h-7" />,
+  내용: <ContentIcon className="w-7 h-7" />,
+  재료: <IngredientsIcon className="w-7 h-7" />,
+  조리순서: <OrdersIcon className="w-7 h-7" />,
+  인원수: <MemberIcon className="w-7 h-7" />,
+};
 
 interface PostCardsProps {
-  title: string;
+  title: keyof typeof iconSelect;
   description: string;
   isEmpty: boolean;
 }
@@ -12,7 +28,7 @@ const PostCard = ({ title, description, isEmpty }: PostCardsProps) => {
       className={`flex flex-1 items-center justify-between gap-2 p-5 pc:p-7 border-2 rounded-lg border-positive-300 hover:bg-positive-50 active:bg-positive-100 text-sm pc:text-md font-semibold shadow-md ${!isEmpty ? "bg-positive-200" : ""}`}
     >
       <div className="flex items-center gap-2 w-28">
-        <AlarmIcon className="w-6 h-6" />
+        {iconSelect[title]}
         <div className="w-full">{title}</div>
       </div>
       <div className="flex w-48 pc:w-64">
