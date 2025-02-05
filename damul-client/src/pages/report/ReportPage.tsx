c6b37c1d26e475ae3f566eae749974c0c6cf2aea
전종우 <jinlaove17@naver.com>
2025-02-05 23:51:16 +0900

    Design : Change report layouts [전종우]

--- damul-client/src/pages/report/ReportPage.tsx
+++ damul-client/src/pages/report/ReportPage.tsx
@@ -1,5 +1,4 @@
 import DamulButton from "@/components/common/DamulButton";
-import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
   Select,
@@ -11,46 +10,47 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
-import { ChangeEvent, FormEvent, useRef, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 interface ReportData {
   reportCategoryId: number;
-  reportType: string;
+  contentType: string;
   targetId: number;
-  reportImageFile: File | null;
   description: string;
 }
 
 const ReportPage = () => {
   const [reportData, setReportData] = useState<ReportData>({
-    reportCategoryId: 0,
-    reportType: "레시피",
+    reportCategoryId: 1,
+    contentType: "",
     targetId: 0,
-    reportImageFile: null,
     description: "",
   });
-  const fileInput = useRef<HTMLInputElement>(null);
 
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    alert("신고처리 되었습니다.");
   };
 
-  const onChangeFile = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files.length > 0) {
-      setReportData({ ...reportData, reportImageFile: e.target.files[0] });
-    }
-  };
-
-  const onAddFile = () => {
-    if (fileInput.current) {
-      fileInput.current.click();
-    }
+  const onChangeReportData = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    console.log(e.target.name, e.target.value);
+    setReportData({ ...reportData, [e.target.name]: e.target.value });
   };
 
   return (
     <div className="px-10 py-8">
       <h1 className="text-xl font-black text-normal-700">신고 접수하기</h1>
-      <form onSubmit={onSubmit} className="flex flex-col gap-10 mt-3">
+      <p className="text-sm text-negative-400">
+        허위로 신고할 경우 서비스 이용이 제한될 수 있습니다.
+      </p>
+      <form onSubmit={onSubmit} className="flex flex-col gap-10 mt-8">
+        <div>
+          <p className="text-sm text-positive-400 font-bold">신고 대상</p>
+          <p>토마토러버전종우</p>
+        </div>
+
         <div className="flex flex-col justify-end w-full">
           <Label
             htmlFor="nickname"
@@ -59,6 +59,7 @@ const ReportPage = () => {
             신고 분류
           </Label>
           <Select
+            name="reportCategoryId"
             value={String(reportData.reportCategoryId)}
             onValueChange={(value) =>
               setReportData({
@@ -66,76 +67,70 @@ const ReportPage = () => {
                 reportCategoryId: parseInt(value),
               })
             }
+            required
           >
-            <SelectTrigger className="w-28">
+            <SelectTrigger className="w-36">
               <SelectValue placeholder="정렬 방식" />
             </SelectTrigger>
             <SelectContent>
               <SelectGroup>
-                <SelectLabel>분류</SelectLabel>
                 <SelectItem
                   className="data-[highlighted]:bg-positive-50 data-[state=checked]:text-positive-500"
-                  value="0"
+                  value="1"
                 >
                   도배
                 </SelectItem>
                 <SelectItem
                   className="data-[highlighted]:bg-positive-50 data-[state=checked]:text-positive-500"
-                  value="1"
+                  value="2"
                 >
                   욕설
                 </SelectItem>
                 <SelectItem
                   className="data-[highlighted]:bg-positive-50 data-[state=checked]:text-positive-500"
-                  value="2"
+                  value="3"
                 >
                   사행성
+                </SelectItem>
+                <SelectItem
+                  className="data-[highlighted]:bg-positive-50 data-[state=checked]:text-positive-500"
+                  value="4"
+                >
+                  개인정보 침해
+                </SelectItem>
+                <SelectItem
+                  className="data-[highlighted]:bg-positive-50 data-[state=checked]:text-positive-500"
+                  value="5"
+                >
+                  기타
                 </SelectItem>
               </SelectGroup>
             </SelectContent>
           </Select>
-        </div>
-
-        <div className="flex w-full items-end space-x-3">
-          <div className="flex flex-col justify-end w-full">
-            <p className="text-sm text-positive-400 font-bold">스크린샷</p>
-            <p className="flex items-center h-10 px-3 rounded-lg border border-normal-200">
-              {reportData.reportImageFile?.name}
-            </p>
-            <input
-              type="file"
-              accept="image/*"
-              className="hidden"
-              onChange={onChangeFile}
-              ref={fileInput}
-            />
-          </div>
-
-          <DamulButton variant="positive" onClick={onAddFile}>
-            사진 추가
-          </DamulButton>
+          <input
+            type="hidden"
+            name="reportCategoryId"
+            value={reportData.reportCategoryId || ""}
+            required
+          />
         </div>
 
         <div className="flex flex-col justify-end w-full">
           <Label
-            htmlFor="introduction"
+            htmlFor="description"
             className="text-sm text-positive-400 font-bold"
           >
-            자기소개
+            신고 사유
           </Label>
           <Textarea
-            id="introduction"
-            name="introduction"
-            className="resize-none focus-visible:ring-1 focus-visible:ring-positive-400 focus-visible:ring-offset-0"
+            id="description"
+            name="description"
+            className="h-32 resize-none focus-visible:ring-1 focus-visible:ring-positive-400 focus-visible:ring-offset-0"
             placeholder="신고 내용을 입력해 주세요."
-            // value={userSetting.selfIntroduction}
-            // onChange={handleInput}
+            value={reportData.description}
+            onChange={onChangeReportData}
+            required
           />
-          {/* <p
-            className={`text-end text-xs ${userSetting.selfIntroduction.length === 255 ? "text-negative-400" : "text-normal-400"}`}
-          >
-            {userSetting.selfIntroduction.length} / 255
-          </p> */}
         </div>
 
         <DamulButton
