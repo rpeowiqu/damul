6395cc6119c0b19e0167c859c10d3c13e91b751b
윤혜진 <dbsgpwls0316@naver.com>
2025-02-07 15:25:11 +0900

    Chore: save mid [윤혜진]

--- damul-server/src/main/java/com/damul/api/post/repository/PostImageRepository.java
+++ damul-server/src/main/java/com/damul/api/post/repository/PostImageRepository.java
@@ -1,4 +1,8 @@
 package com.damul.api.post.repository;
 
-public interface PostImageRepository {
+import com.damul.api.post.entity.PostImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostImageRepository extends JpaRepository<PostImage, Integer> {
+
 }
--- damul-server/src/main/java/com/damul/api/post/service/PostService.java
+++ damul-server/src/main/java/com/damul/api/post/service/PostService.java
@@ -1,33 +1,5 @@
 package com.damul.api.post.service;
 
-import com.damul.api.post.dto.response.PostList;
-import com.damul.api.post.entity.Post;
-import io.lettuce.core.dynamic.annotation.Param;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
-
-public interface PostService extends JpaRepository<Post, Integer> {
-
-    // 기본 조회
-    @Query("""
-            SELECT new com.damul.api.post.dto.response.PostList(
-                        p.postId, p.title, p.thumbnailUrl, p.content, 
-                        p.createdAt, p.user.id, p.user.nickname, p.postStatus
-                        )
-            FROM Post p
-            JOIN p.user u
-            WHERE (p.postStatus = "ACTIVE"
-            OR p.postStatus = "COMPLETED")
-            AND (:cursorId = 0 OR p.postId < :cursorId)
-            ORDER BY p.postId DESC
-            LIMIT :size
-            """)
-    List<PostList> findAllPosts(
-            @Param("cursorId") int cursorId,
-            @Param("size") int size
-    );
-
+public interface PostService {
 
 }
