ea05b72b863cdfc1f4c064585b352d1921c91669
전종우 <jinlaove17@naver.com>
2025-02-07 19:12:16 +0900

    Refactor : Refactor DamulInfiniteScrollList component [전종우]

--- damul-client/src/components/common/DamulInfiniteScrollList.tsx
+++ damul-client/src/components/common/DamulInfiniteScrollList.tsx
@@ -1,75 +1,60 @@
 import { ReactNode, useEffect, useRef } from "react";
 import { useInView } from "react-intersection-observer";
 import { useInfiniteQuery } from "@tanstack/react-query";
-import { Divide } from "lucide-react";
 
 interface DamulInfiniteScrollListProps<T> {
   queryKey: string[];
   fetchFn: (pageParam: number) => Promise<T[]>;
-  renderItems: (item: T, index: number) => ReactNode;
-  direction?: "forward" | "backward";
+  initPage?: number;
   loadSize?: number; // 스켈레톤 개수를 출력할 때 사용
+  renderItems: (item: T, index: number) => ReactNode;
+  skeleton?: ReactNode;
   className?: string;
 }
 
 const DamulInfiniteScrollList = <T,>({
   queryKey,
   fetchFn,
+  initPage = 1,
+  loadSize = 1,
   renderItems,
-  direction = "forward",
+  skeleton,
   className,
 }: DamulInfiniteScrollListProps<T>) => {
   const { ref, inView } = useInView({
     triggerOnce: false,
     threshold: 0,
   });
-  const prevScrollHeight = useRef<number>(0);
-  const {
-    data,
-    fetchNextPage,
-    fetchPreviousPage,
-    hasNextPage,
-    hasPreviousPage,
-    isFetchingNextPage,
-    isFetchingPreviousPage,
-  } = useInfiniteQuery({
-    queryKey: [queryKey],
-    queryFn: ({ pageParam = 1 }) => fetchFn(pageParam),
-    initialPageParam: 1,
-    getNextPageParam: (lastPage, allPages) =>
-      lastPage.length > 0 ? allPages.length + 1 : undefined,
-    getPreviousPageParam: (firstPage, allPages) =>
-      firstPage.length > 0 ? allPages.length - 1 : undefined,
-  });
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteQuery({
+      queryKey: [queryKey],
+      queryFn: ({ pageParam = 1 }) => fetchFn(pageParam),
+      initialPageParam: initPage,
+      getNextPageParam: (lastPage, allPages) =>
+        lastPage.length > 0 ? allPages.length + 1 : undefined,
+    });
 
   useEffect(() => {
-    if (inView) {
-      switch (direction) {
-        case "forward":
-          if (hasNextPage) {
-            fetchNextPage();
-          }
-          break;
-        case "backward":
-          if (hasPreviousPage) {
-            fetchPreviousPage();
-          }
-          break;
-      }
+    if (inView && hasNextPage) {
+      fetchNextPage();
     }
   }, [inView]);
 
   return (
     <div className={className}>
-      {direction === "forward" && <div ref={ref}></div>}
-
       {data?.pages.map((page, pageIndex) =>
         page.map((item, index) =>
           renderItems(item, index + pageIndex * page.length),
         ),
       )}
 
-      {direction === "forward" && <div ref={ref}></div>}
+      {isFetchingNextPage ? (
+        Array.from({ length: loadSize }).map((_, index) => (
+          <div key={index}>{skeleton}</div>
+        ))
+      ) : (
+        <div ref={ref}></div>
+      )}
     </div>
   );
 };
--- damul-client/src/pages/TestPage.tsx
+++ damul-client/src/pages/TestPage.tsx
@@ -2,11 +2,18 @@ import DamulInfiniteScrollList from "@/components/common/DamulInfiniteScrollList
 
 const TestPage = () => {
   const fetchItems = async (pageParam: number) => {
-    console.log(pageParam);
+    if (pageParam > 1) {
+      // 스켈레톤을 확인하기 위해, 지연시간을 일부로 추가
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+    }
+
     const response = await fetch(
       `https://jsonplaceholder.typicode.com/posts?_page=${pageParam}&_limit=10`,
     );
-    if (!response.ok) throw new Error("데이터를 불러오지 못했습니다.");
+    if (!response.ok) {
+      throw new Error("데이터를 불러오지 못했습니다.");
+    }
+
     return response.json();
   };
 
@@ -14,12 +21,16 @@ const TestPage = () => {
     <DamulInfiniteScrollList
       queryKey={["posts"]}
       fetchFn={fetchItems}
+      loadSize={5}
       renderItems={(item: { id: number; title: string; body: string }) => (
-        <div key={item.id} className="p-3 my-2 rounded-md bg-normal-50">
+        <div key={item.id} className="p-3 mb-2 rounded-md bg-normal-50">
           <h3 className="font-semibold">{item.title}</h3>
           <p>{item.body}</p>
         </div>
       )}
+      skeleton={
+        <div className="h-24 mb-2 animate-pulse bg-normal-100 rounded" />
+      }
     />
   );
 };
