5b5ea0c61da37a4b6e7488167843397f8ea1c25a
신성우 <asdfgg77@naver.com>
2025-02-10 17:13:28 +0900

    feat : add IngredientsRegisterPage data flow logic [신성우]

--- damul-client/src/components/home/IngredientItem.tsx
+++ damul-client/src/components/home/IngredientItem.tsx
@@ -1,35 +1,83 @@
-const IngredientItem = () => {
+import { CATEGORYNUMBER, CATEGORYNAME } from "@/constants/category";
+import { STORAGE_TYPE_MAP, STORAGE_TYPE_CONST } from "@/constants/storage";
+
+interface IngredientItemProps {
+  ingredient?: {
+    ingredientName: string;
+    productPrice: number;
+    categoryId: number;
+    dueDate: string;
+    ingredientStorage: keyof typeof STORAGE_TYPE_CONST;
+  };
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onDelete: () => void;
+}
+
+const IngredientItem = ({
+  ingredient,
+  onChange,
+  onDelete,
+}: IngredientItemProps) => {
   return (
     <div className="flex w-full border-1 p-4 rounded-lg gap-5">
       <div className="flex justify-center items-center px-2">
         <button
           type="button"
-          onClick={() => {}}
+          onClick={onDelete}
           className="flex items-center justify-center w-5 h-5 rounded-full text-negative-600 hover:negative-700 border-2 border-negative-600 text-xl font-semibold"
         >
           -
         </button>
       </div>
       <div className="flex flex-col w-full gap-2">
-        <div className="flex w-full justify-between">
+        <div className="flex gap-2 w-full justify-between">
           <label className="w-20">상품명</label>
-          <input type="text" className="border-1 w-full" />
+          <input
+            type="text"
+            className="border-1 w-full text-right focus:outline-positive-300"
+            value={ingredient && ingredient.ingredientName}
+            onChange={onChange}
+          />
         </div>
-        <div className="flex w-full justify-between">
+        <div className="flex gap-2 w-full justify-between">
           <label className="w-20">분류</label>
-          <input type="text" className="border-1 w-full" />
+          <input
+            type="text"
+            className="border-1 w-full text-right focus:outline-positive-300"
+            onChange={onChange}
+            value={
+              ingredient && CATEGORYNAME[CATEGORYNUMBER[ingredient.categoryId]]
+            }
+          />
         </div>
-        <div className="flex w-full justify-between">
+        <div className="flex gap-2 w-full justify-between">
           <label className="w-20">가격</label>
-          <input type="text" className="border-1 w-full" />
+          <input
+            type="text"
+            className="border-1 w-full text-right focus:outline-positive-300"
+            onChange={onChange}
+            value={ingredient && ingredient.productPrice}
+          />
         </div>
-        <div className="flex w-full justify-between">
+        <div className="flex gap-2 w-full justify-between">
           <label className="w-20">소비기한</label>
-          <input type="text" className="border-1 w-full" />
+          <input
+            type="text"
+            className="border-1 w-full  text-right focus:outline-positive-300"
+            onChange={onChange}
+            value={ingredient && ingredient.dueDate}
+          />
         </div>
-        <div className="flex w-full justify-between">
+        <div className="flex gap-2 w-full justify-between">
           <label className="w-20">보관장소</label>
-          <input type="text" className="border-1 w-full" />
+          <input
+            type="text"
+            className="border-1 w-full text-right focus:outline-positive-300"
+            onChange={onChange}
+            value={
+              ingredient && STORAGE_TYPE_CONST[ingredient.ingredientStorage]
+            }
+          />
         </div>
       </div>
     </div>
--- damul-client/src/pages/home/HomeIngredientsRegisterPage.tsx
+++ damul-client/src/pages/home/HomeIngredientsRegisterPage.tsx
@@ -1,8 +1,75 @@
-import SubmitButton from "@/components/community/SubmitButton";
+import DamulButton from "@/components/common/DamulButton";
 import IngredientItem from "@/components/home/IngredientItem";
 import PlusIcon from "@/components/svg/PlusIcon";
+import { useState } from "react";
+
+const initialData = {
+  purchaseAt: "2021-10-10",
+  storeName: "홈플러스",
+  userIngredients: [
+    {
+      ingredientName: "양파",
+      productPrice: 1000,
+      categoryId: 1,
+      dueDate: "2021-10-20",
+      ingredientStorage: "FREEZER",
+    },
+  ],
+};
 
 const HomeIngredientsRegisterPage = () => {
+  const [ingredientRegisterData, setIngredientRegisterData] =
+    useState(initialData);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setIngredientRegisterData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleIngredientChange = (index, key, value) => {
+    setIngredientRegisterData((prev) => {
+      const updatedIngredients = [...prev.userIngredients];
+      updatedIngredients[index] = {
+        ...updatedIngredients[index],
+        [key]: value,
+      };
+      return { ...prev, userIngredients: updatedIngredients };
+    });
+  };
+
+  const addIngredient = () => {
+    setIngredientRegisterData((prev) => ({
+      ...prev,
+      userIngredients: [
+        ...prev.userIngredients,
+        {
+          ingredientName: "",
+          productPrice: 0,
+          categoryId: 0,
+          dueDate: "",
+          ingredientStorage: "FRIDGE",
+        },
+      ],
+    }));
+  };
+
+  const removeIngredient = (index: number) => {
+    setIngredientRegisterData((prev) => {
+      const updatedIngredients = prev.userIngredients.filter(
+        (_, i) => i !== index,
+      );
+      return { ...prev, userIngredients: updatedIngredients };
+    });
+  };
+
+  const totalAmount = ingredientRegisterData.userIngredients.reduce(
+    (sum, item) => sum + item.productPrice,
+    0,
+  );
+
   return (
     <div className="flex flex-col p-5">
       <div className="flex w-full gap-6 items-center text-xl font-bold">
@@ -21,26 +88,52 @@ const HomeIngredientsRegisterPage = () => {
 
       <div className="my-4 flex flex-col gap-4">
         <div className="flex w-full gap-2">
-          <div className="w-full">
-            <p>매장명</p>
-            <input type="text" className="border-1 w-full" />
+          <div className="w-full flex flex-col gap-2">
+            <label htmlFor="shop">매장명</label>
+            <input
+              name="storeName"
+              id="shop"
+              type="text"
+              value={ingredientRegisterData.storeName}
+              onChange={handleInputChange}
+              className="border-1 w-full focus:outline-positive-300"
+            />
           </div>
-          <div className="w-full">
-            <p>날짜</p>
-            <input type="date" className="border-1" />
+          <div className="w-full flex flex-col gap-2">
+            <label>날짜</label>
+            <input
+              name="purchaseAt"
+              type="date"
+              value={ingredientRegisterData.purchaseAt}
+              onChange={handleInputChange}
+              className="border-1 focus:outline-positive-300"
+            />
           </div>
         </div>
         <div className="flex flex-col gap-4 h-[200px] overflow-y-auto">
-          {Array.from({ length: 3 }).map((_, index) => {
-            return <IngredientItem key={index} />;
-          })}
+          {ingredientRegisterData.userIngredients.map((ingredient, index) => (
+            <IngredientItem
+              key={index}
+              ingredient={ingredient}
+              onChange={(key, value) =>
+                handleIngredientChange(index, key, value)
+              }
+              onDelete={() => removeIngredient(index)}
+            />
+          ))}
         </div>
       </div>
-      <div className="flex justify-end gap-2 font-bold ">
-        총 구매 금액: <span className="text-positive-300">25,400</span>원
+      <div className="flex justify-end gap-2 font-bold">
+        총 구매 금액: <span className="text-positive-300">{totalAmount}</span>원
       </div>
-      <div className="flex justify-center py-3"></div>
-      <SubmitButton></SubmitButton>
+      <div className="flex justify-center p-4">
+        <DamulButton className="bg-normal-200" onClick={addIngredient}>
+          +
+        </DamulButton>
+      </div>
+      <DamulButton className="bg-positive-300 hover:bg-positive-300/60">
+        등록
+      </DamulButton>
     </div>
   );
 };
