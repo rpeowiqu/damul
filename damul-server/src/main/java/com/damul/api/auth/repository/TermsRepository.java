6e211e565ea5b64b89b7e8c9c56c81b108b72790
지유림 <zyu22@naver.com>
2025-02-11 17:12:40 +0900

    Fix : Modify temp_token filter [지유림]

--- damul-server/src/main/java/com/damul/api/auth/repository/TermsRepository.java
+++ damul-server/src/main/java/com/damul/api/auth/repository/TermsRepository.java
@@ -1,12 +1,14 @@
 package com.damul.api.auth.repository;
 
 import com.damul.api.auth.entity.Terms;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface TermsRepository extends JpaRepository<Terms, Long> {
+public interface TermsRepository extends JpaRepository<Terms, Integer> {
+    @Cacheable("termsCache")
     List<Terms> findAll();
 }
