f86cce9d29bfa065b030d3f021875ee264c13433
전종우 <jinlaove17@naver.com>
2025-02-15 23:37:41 +0900

    Feat : Complete statistics history page api [전종우]

--- damul-client/src/types/statistics.ts
+++ damul-client/src/types/statistics.ts
@@ -3,3 +3,26 @@ export interface IngredientTrendInfo {
   ingredientName: "string";
   ingredientCode: number;
 }
+
+export interface DailyReceiptInfo {
+  dayOfMonth: number;
+  receiptIds: number[];
+}
+
+export interface PurchaseHistory {
+  monthlyTotalAmount: number;
+  comparedPreviousMonth: number;
+  dailyReceiptInfoList: DailyReceiptInfo[];
+}
+
+export interface ReceiptDetail {
+  productName: string;
+  category_name: string;
+  price: number;
+}
+
+export interface Receipt {
+  storeName: string;
+  receiptDetails: ReceiptDetail[];
+  totalPrice: number;
+}
