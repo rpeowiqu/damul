be8a5bb27aec8ead759eef271845539bcb745ec9
윤혜진 <dbsgpwls0316@naver.com>
2025-02-18 14:21:59 +0900

    Feat: Add kogpt2 service [윤혜진]

--- damul-ocr/main.py
+++ damul-ocr/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, File, UploadFile
 from services.ocr_service import ocr_service_execution
 from services.gpt_service import gpt_service_execution
+from services.kogpt2_service import kogpt_service_execution
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI() # 인스턴스 생성
@@ -43,3 +44,30 @@ async def user_ingredients_ocr(image: UploadFile = File(...)):
         import traceback
         error_details = traceback.format_exc()
         return {'ERROR': 'INTERNAL_SERVER_ERROR', 'DETAILS': str(error_details)}
+
+
+@app.post('/api/v1/ocrai')
+async def user_ingredients_ocr(image: UploadFile = File(...)):
+    try:
+        image_bytes = await image.read()
+
+        # ocr
+        extracted_text = await ocr_service_execution(image_bytes)
+        if not extracted_text[0]:
+            return { 'ERROR' : extracted_text[1] }
+        extracted_text = " ".join(extracted_text)
+        print(extracted_text)
+
+        # gpt
+        result_json = await kogpt_service_execution(extracted_text)
+
+
+        # 응답
+        # return extracted_text
+        return result_json
+
+    except Exception as e:
+        import traceback
+        error_details = traceback.format_exc()
+        return {'ERROR': 'INTERNAL_SERVER_ERROR', 'DETAILS': str(error_details)}
+
