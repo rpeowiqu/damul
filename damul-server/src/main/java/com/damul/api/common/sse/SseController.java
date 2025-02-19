d971671b35df24b6142b1c7925646e323d27dfec
한재서 <hanahyun1@naver.com>
2025-02-19 14:32:22 +0900

    Fix: Bugfix with SSE service[한재서]

--- damul-server/src/main/java/com/damul/api/common/sse/SseController.java
+++ damul-server/src/main/java/com/damul/api/common/sse/SseController.java
@@ -3,6 +3,7 @@ package com.damul.api.common.sse;
 import com.damul.api.common.sse.service.SseService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,7 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 public class SseController {
     private final SseService sseService;
 
-    @GetMapping("/connect/{userId}")
+    @GetMapping(value = "/connect/{userId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter connect(@PathVariable int userId) {
         log.info("SSE 연결 요청 - userId: {}", userId);
         return sseService.createEmitter(userId);
